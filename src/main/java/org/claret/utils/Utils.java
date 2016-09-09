383d28d4eaf2b836f8fc48903ed162fd69de4757
samlv(吕亚辉) <samlv@tencent.com>
2016-09-09 20:33:42 +0800

    add log4j

--- src/main/java/org/claret/utils/Utils.java
+++ src/main/java/org/claret/utils/Utils.java
@@ -1,9 +1,15 @@
 package org.claret.utils;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author samlv
  * Created by samlv on 2016/8/30.
  */
 abstract public class Utils {
+    protected static Logger logger ;
 
+    static {
+        logger = Logger.getLogger(Utils.class);
+    }
 }
