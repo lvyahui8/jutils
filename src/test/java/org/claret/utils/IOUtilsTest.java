d0aea9c3622139113dce7a6f043e970ebcf72bdf
samlv(吕亚辉) <samlv@tencent.com>
2016-09-09 13:33:50 +0800

    文件复制完成

--- src/test/java/org/claret/utils/IOUtilsTest.java
+++ src/test/java/org/claret/utils/IOUtilsTest.java
@@ -21,11 +21,21 @@ public class IOUtilsTest extends TestCase {
     }
 
     public void testCopy() throws Exception {
-        IOUtils.copy("E:/www/ruochen/trunk/uploads/pdf/a-lvyahui-resume-aboutMeV8-4.pdf","F:/tmp/resume.pdf",false);
-        IOUtils.copy("E:/www/ruochen/trunk/uploads/pdf/a-lvyahui-resume-aboutMeV8-4.pdf","F:/tmp/resume.pdf");
-        IOUtils.copy("C:/soft/QQSnapShot.exe","F:/tmp",false);
-        IOUtils.copy("C:/soft/QQSnapShot.exe","F:/tmp");
-        IOUtils.copy("C:/soft/apache-maven-3.3.9/bin","F:/tmp/maven",false);
-        IOUtils.copy("C:/soft/apache-maven-3.3.9/bin","F:/tmp/maven");
+        // IOUtils.copy("E:/www/ruochen/trunk/uploads/pdf/a-lvyahui-resume-aboutMeV8-4.pdf","F:/tmp/resume.pdf",false);
+        // IOUtils.copy("E:/www/ruochen/trunk/uploads/pdf/a-lvyahui-resume-aboutMeV8-4.pdf","F:/tmp/resume.pdf");
+        // IOUtils.copy("C:/soft/QQSnapShot.exe","F:/tmp",false);
+        // IOUtils.copy("C:/soft/QQSnapShot.exe","F:/tmp");
+        // IOUtils.copy("C:/soft/apache-maven-3.3.9/bin","F:/tmp/maven",false);
+        // IOUtils.copy("C:/soft/apache-maven-3.3.9/bin","F:/tmp/maven");
+
+        // 文件复制到目标文件
+        // IOUtils.copy("C:/Windows/System32/drivers/etc/hosts","C:/tmp/hosts");
+
+        // 文件复制到目标目录
+        // IOUtils.copy("C:/Windows/System32/drivers/etc/hosts","C:/tmp");
+
+        // 目录复制到一个不存在的目录
+        // IOUtils.copy("C:/Windows/System32/drivers/UMDF","C:/tmp/UMDF");
+
     }
 }
---
+++ src/test/java/org/claret/utils/TaskUtilsTest.java
@@ -0,0 +1,14 @@
+package org.claret.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * @author lvyahui
+ * @since 2016/9/4 22:14.
+ */
+public class TaskUtilsTest extends TestCase {
+
+    public void testSubmit() throws Exception {
+        TaskUtils.submit();
+    }
+}
